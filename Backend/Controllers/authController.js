2bcb08c043aa40d84788cd6537cdcf690ba4f833
manaf <manafbidal@gmail.com>
2024-10-09 22:43:11 +0330

    Update user authentication methods

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -6,20 +6,20 @@ class AuthController  {
         try {
             const {fullname , username , password } = req.body;
             if(!fullname || !username || !password) {
-                return res.status(400).json({status : 'error' , message : "همه فیلد ها الزامی هستند"})
+                return res.json({status : 'error' , message : "همه فیلد ها الزامی هستند"})
             }
             if(fullname.length <  5 ) {
-                return res.status(400).json({status : 'error' , message : "طول نام کامل شما نباید از  5 کاراکتر کمتر باشد"})
+                return res.json({status : 'error' , message : "طول نام کامل شما نباید از  5 کاراکتر کمتر باشد"})
             }
             if(username.length < 3 ) {
-                return res.status(400).json({status : 'error' , message : "طول نام کاربری شما نباید از  3 کاراکتر کمتر باشد"})
+                return res.json({status : 'error' , message : "طول نام کاربری شما نباید از  3 کاراکتر کمتر باشد"})
             }
             if(password.length < 6 ) {
-                return res.status(400).json({status : 'error' , message : "طول گذرواژه نباید از 6 کاراکتر کمتر باشد"})
+                return res.json({status : 'error' , message : "طول گذرواژه نباید از 6 کاراکتر کمتر باشد"})
             }
             const existsUser = await User.findOne({username})
             if(existsUser) {
-                return res.status(400).json({status : 'error' , message :"نام کاربری نامعتبر است"})
+                return res.json({status : 'error' , message :"نام کاربری نامعتبر است"})
             }
             const hashPassword = await bcrypt.hash(password , 10)
             const newUser = new User({
@@ -39,21 +39,21 @@ class AuthController  {
         try {
             const {username , password } = req.body;
             if( !username || !password) {
-                return res.status(400).json({status : 'error' , message : "همه فیلد ها الزامی هستند"})
+                return res.json({status : 'error' , message : "همه فیلد ها الزامی هستند"})
             }
             if(username.length < 3 ) {
-                return res.status(400).json({status : 'error' , message : "طول نام کاربری شما نباید از  3 کاراکتر کمتر باشد"})
+                return res.json({status : 'error' , message : "طول نام کاربری شما نباید از  3 کاراکتر کمتر باشد"})
             }
             if(password.length < 6 ) {
-                return res.status(400).json({status : 'error' , message : "طول گذرواژه نباید از 6 کاراکتر کمتر باشد"})
+                return res.json({status : 'error' , message : "طول گذرواژه نباید از 6 کاراکتر کمتر باشد"})
             }
             const user = await User.findOne({username})
             if(!user) {
-                return res.status(404).json({status : 'error' , message :"کابری یافت نشد"})
+                return res.json({status : 'error' , message :"کابری یافت نشد"})
             }
             const comparePassword = await bcrypt.compare(password , user.password )
             if(!comparePassword ){
-                return res.status(400).json({status : 'error' , message :"نام کاربری یا رمز عبور صحیح نیست"})
+                return res.json({status : 'error' , message :"نام کاربری یا رمز عبور صحیح نیست"})
             }
             const token = await generateTokenAndSetCookie(user , res)
             res.json({status :'success' , user : { id : user._id  , fullname : user.fullname , username : user.username , avatar : user.avatar , token }})
@@ -74,7 +74,7 @@ class AuthController  {
         try {
             const id = req.user._id;
             const user = await User.findById(id)
-            if(!user) return res.status(401) ;
+            if(!user) return ;
             return res.json({status : 'success' , user})
         } catch (error) {
             next(error)
